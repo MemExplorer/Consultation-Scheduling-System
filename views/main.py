1ebba34d6fbb9165652f10fbbae382f67434bf8a
Joe Marvin Tabañera <joemarvin.dev@gmail.com>
2023-06-06 06:54:44 +0800

    Added spacing

--- views/main.py
+++ views/main.py
@@ -13,41 +13,54 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_initialWindow(object):
     def setupUi(self, initialWindow):
+
         initialWindow.setObjectName("initialWindow")
         initialWindow.resize(626, 504)
         initialWindow.setMaximumSize(QtCore.QSize(626, 504))
         initialWindow.setDocumentMode(False)
+
         self.centralwidget = QtWidgets.QWidget(initialWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(130, 340, 171, 41))
         self.pushButton.setObjectName("pushButton")
+
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setGeometry(QtCore.QRect(330, 340, 171, 41))
         self.pushButton_2.setObjectName("pushButton_2")
+
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_3.setGeometry(QtCore.QRect(130, 400, 371, 41))
         self.pushButton_3.setObjectName("pushButton_3")
+
         self.Title = QtWidgets.QLabel(self.centralwidget)
         self.Title.setGeometry(QtCore.QRect(110, 40, 391, 51))
+
         font = QtGui.QFont()
         font.setPointSize(28)
+
         self.Title.setFont(font)
         self.Title.setTextFormat(QtCore.Qt.RichText)
         self.Title.setScaledContents(False)
         self.Title.setAlignment(QtCore.Qt.AlignCenter)
         self.Title.setObjectName("Title")
+
         initialWindow.setCentralWidget(self.centralwidget)
+
         self.statusbar = QtWidgets.QStatusBar(initialWindow)
         self.statusbar.setObjectName("statusbar")
+
         initialWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(initialWindow)
+        
         QtCore.QMetaObject.connectSlotsByName(initialWindow)
 
     def retranslateUi(self, initialWindow):
         _translate = QtCore.QCoreApplication.translate
         initialWindow.setWindowTitle(_translate("initialWindow", "Consultation System"))
+        
         self.pushButton.setText(_translate("initialWindow", "Sign up"))
         self.pushButton_2.setText(_translate("initialWindow", "Login"))
         self.pushButton_3.setText(_translate("initialWindow", "Forgot password?"))
