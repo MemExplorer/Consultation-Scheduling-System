1534f79ea5fc0ae1f17111ce5a32702de2e2e7b6
Joe Marvin Tabañera <joemarvin.dev@gmail.com>
2023-06-05 14:48:03 +0800

    Added database query

---
+++ models/test_auth.py
@@ -0,0 +1,36 @@
+""" Reference
+Test Authentication on XAMPP and mysql-connector module.
+Ignore on build.
+"""
+import mysql.connector
+from mysql.connector import Error
+
+# Database connection protocol
+db = mysql.connector.connect(
+    host="localhost",
+    database="test",
+    user="root",
+    passwd=""
+)
+
+# Handle error on queries
+try:
+
+    # SQL Query
+    query = "SELECT * FROM tbl_test"
+
+    # Database connection using cursor. I don't know how it work, but it works.
+    cursor = db.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    print(f"Number of records in the table:{cursor.rowcount}\nRecords: {results}")
+
+except Error as error:
+    print(f"Failed: {error}")
+
+finally:
+    # Close once connected.
+    if db.is_connected():
+        cursor.close()
+        db.close()
+        print("Connection Closed")
