0b264177c28e9633113ca06516867b7f279432e6
Joe Marvin Tabañera <joemarvin.dev@gmail.com>
2023-06-15 09:22:48 +0800

    added date in sql query

--- models/csystem.sql
+++ models/csystem.sql
@@ -5,4 +5,4 @@ USE db_csystem;
 CREATE TABLE tbl_csystem(account_id INT NOT NULL PRIMARY KEY auto_increment, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL,
  email VARCHAR(255) NOT NULL, password VARCHAR(30) NOT NULL, category VARCHAR(30) NOT NULL);
  
- CREATE TABLE tbl_task(task_id INT NOT NULL PRIMARY KEY auto_increment, task_title VARCHAR(255) NOT NULL, task_description VARCHAR(255) NOT NULL);
+ CREATE TABLE tbl_task(task_id INT NOT NULL PRIMARY KEY auto_increment, task_title VARCHAR(255) NOT NULL, task_description VARCHAR(255) NOT NULL, date DATETIME);
