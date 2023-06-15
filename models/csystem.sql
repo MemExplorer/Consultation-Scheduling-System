5ad2ae27b5737532f88dd9869d23b1b4aabec1d9
Joe Marvin Tabañera <joemarvin.dev@gmail.com>
2023-06-15 09:28:36 +0800

    Fix test_auth for database query

--- models/csystem.sql
+++ models/csystem.sql
@@ -2,7 +2,9 @@ DROP DATABASE db_csystem;
 CREATE DATABASE db_csystem;
 USE db_csystem;
 
-CREATE TABLE tbl_csystem(account_id INT NOT NULL PRIMARY KEY auto_increment, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL,
+CREATE TABLE tbl_accounts(account_id INT NOT NULL PRIMARY KEY auto_increment, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL,
  email VARCHAR(255) NOT NULL, password VARCHAR(30) NOT NULL, category VARCHAR(30) NOT NULL);
  
  CREATE TABLE tbl_task(task_id INT NOT NULL PRIMARY KEY auto_increment, task_title VARCHAR(255) NOT NULL, task_description VARCHAR(255) NOT NULL, date DATETIME);
+ 
+ INSERT INTO tbl_accounts VALUES(1 ,"John", "Doe", "johndoe@gmail.com", "test", "student");
