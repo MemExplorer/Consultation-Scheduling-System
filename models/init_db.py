e88312e954701b0d512b4b8e04b3f2080ea1faea
Joe Marvin Tabañera <joemarvin.dev@gmail.com>
2023-06-17 13:31:55 +0800

    Resolved pre-defined database connection issue

--- models/init_db.py
+++ models/init_db.py
@@ -1,55 +1,23 @@
 import mysql.connector
 
 try:
-    # Try to connect to the existing db_csystem database
-    db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="test",
-        database="db_csystem"
-    )
-    db_connected = True
-except mysql.connector.Error as error:
-    db_connected = False
-
-if db_connected:
-    # The db_csystem database already exists, proceed with the code as before
-    cursor = db.cursor()
-
-    # Read the contents of data.sql
-    with open('/models/csystem.sql', 'r') as file:
-        sql_script = file.read()
-
-    # Execute the SQL commands from data.sql
-    cursor.execute(sql_script)
-
-    # Commit the changes to the database
-    db.commit()
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-else:
-    # Create a new db_csystem database and populate it with table and values from csystem.sql
+    # Connect to the database
     db = mysql.connector.connect(
         host="localhost",
         user="root",
         password="test"
     )
+
+    # Create a cursor object
     cursor = db.cursor()
 
-    # Create the db_csystem database
-    cursor.execute("CREATE DATABASE db_csystem")
-
-    # Use the db_csystem database
-    cursor.execute("USE db_csystem")
-
-    # Read the contents of data.sql
-    with open('/models/csystem.sql', 'r') as file:
+    # Read the contents of csystem.sql
+    with open('./models/csystem.sql', 'r') as file:
         sql_script = file.read()
 
-    # Execute the SQL commands from data.sql
-    cursor.execute(sql_script)
+    # Execute the SQL commands from csystem.sql
+    for result in cursor.execute(sql_script, multi=True):
+        pass
 
     # Commit the changes to the database
     db.commit()
@@ -57,3 +25,8 @@ else:
     # Close the cursor and database connection
     cursor.close()
     db.close()
+
+    print("Database initialization completed successfully.")
+    
+except mysql.connector.Error as error:
+    print(f"Error during database initialization: {error}")
