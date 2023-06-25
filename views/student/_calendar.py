aa1c71bf66bb0658df9640b5a445155a98ebcb77
Joe Marvin Tabañera <joemarvin.dev@gmail.com>
2023-06-25 17:25:20 +0800

    Modified UI calendar.py in student.py

--- views/student/_calendar.py
+++ views/student/_calendar.py
@@ -19,13 +19,14 @@ class CalendarFrame(ctk.CTkFrame):
 
         # load images with light and dark mode image
         """ File directory pathing for images """
+        self.CalendarImage = ctk.CTkImage(light_image=res.fetch_image(res.images.nav_ico.calendar_dark), dark_image=res.fetch_image(res.images.nav_ico.calendar_light), size=(20, 20))
         self.NotifImage = ctk.CTkImage(light_image=res.fetch_image(res.images.nav_ico.notif_dark), dark_image=res.fetch_image(res.images.nav_ico.notif_light), size=(20, 20))
         self.AlertNotifImage = ctk.CTkImage(light_image=res.fetch_image(res.images.nav_ico.alert_notif_dark), dark_image=res.fetch_image(res.images.nav_ico.alert_notif_light), size=(20, 20))
         """ End of resource pathing """
 
         # Styling as row-stretch
         self.grid_columnconfigure(0, weight=5)
-        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
 
         # Theme design, because I can't setup json file for custom theme installation using set_default_theme.
         self.THEME_GREEN = self.master.THEME_GREEN
@@ -38,19 +39,32 @@ class CalendarFrame(ctk.CTkFrame):
         self.TitleWrapper.grid(row=0, columnspan=1, padx=20, pady=10, ipady=10, sticky="nsew")
         self.TitleWrapper.grid_columnconfigure(0, weight=1)
 
+        # CalendarUtilitiesWrapper
+        self.CaledanrUtilitiesWrapper = ctk.CTkFrame(self, fg_color=self.THEME_GREEN, height=50)
+        self.CaledanrUtilitiesWrapper.grid(row=1, column=0, padx=20, pady=5, sticky="nsew")
+        self.CaledanrUtilitiesWrapper.grid_columnconfigure(1, weight=1)
+
+        # MainWrapper
+        self.MainWrapper = ctk.CTkFrame(master=self, fg_color=self.THEME_GREEN)
+        self.MainWrapper.grid(row=2, columnspan=1, padx=20, pady=10, sticky="nsew")
+        self.MainWrapper.grid_columnconfigure(0, weight=1)
+
         # TitleWrapper | TitleWrapper Welcome Message
         self.TitleLabel = ctk.CTkLabel(self.TitleWrapper, text=f"Calendar View", text_color="black", font=ctk.CTkFont(family="Poppins", size=24, weight='bold'))
-        self.TitleLabel.grid(row=0, column=0, pady=20, padx=10, sticky="w")
+        self.TitleLabel.grid(row=0, column=0,  padx=10, pady=10, sticky="w")
 
         # TitleWrapper | Notifications
         self.NotifIcon = ctk.CTkButton(self.TitleWrapper, text=None, image=self.NotifImage, width=5, fg_color="transparent", hover_color="#Fdf0d5")
         self.NotifIcon.grid(row=0, column=1, padx=10, pady=10, sticky="e")
 
-        # MainWrapper
-        self.MainWrapper = ctk.CTkFrame(master=self, fg_color=self.THEME_GREEN)
-        self.MainWrapper.grid(row=1, columnspan=1, padx=20, pady=10, sticky="nsew")
-        self.MainWrapper.grid_columnconfigure(0, weight=1)
+        # CalendarUtilitiesWrapper | Calendar Teacher Searching Utility
+        self.FacultyPicker = ctk.CTkOptionMenu(self.CaledanrUtilitiesWrapper, fg_color=self.THEME_DARKGREEN, dropdown_fg_color=self.THEME_DARKGREEN, button_color=self.THEME_DARKGREEN, button_hover_color=self.THEME_DARKGREEN, text_color=self.DEFAULT)
+        self.FacultyPicker.grid(row=0, column=0, padx=10, pady=5, ipady=5, ipadx=5, sticky="w")
 
-        # Add calendar widget
-        self.calendar = Calendar(self.MainWrapper)
+        # CalendarUtilitiesWrapper | Calendar Scheduling Creation Dialog
+        self.ScheduleButton = ctk.CTkButton(self.CaledanrUtilitiesWrapper, image=self.CalendarImage, text="Schedule a Consultation", font=ctk.CTkFont(family="Poppins", size=14))
+        self.ScheduleButton.grid(row=0, column=1, padx=10, pady=5, ipady=5, ipadx=5, sticky="e")
+
+        # MainWrapper | Add calendar widget
+        self.calendar = Calendar(self.MainWrapper, selectmode="day", year=datetime.datetime.now().year, month=datetime.datetime.now().month, day=datetime.datetime.now().day)
         self.calendar.pack(expand=True, fill=tk.BOTH)
